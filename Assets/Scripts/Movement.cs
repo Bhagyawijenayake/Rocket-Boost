cfe71ac732753c32a7603302a1acd66bef3093fd
Bhagya Wijenayake <bhagyasudaraka98@gmail.com>
2023-12-03 16:24:48 +0530

    inputs created

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    Rigidbody rb;
+    [SerializeField] float mainThrust = 100f;
+    [SerializeField] float rotationThrust = 2f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        }
+    }
+
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotationThrust);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-rotationThrust);
+        }
+    }
+
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        rb.freezeRotation=true; //freezing rotation so we can manually rotate
+        transform.Rotate(Vector3.forward  * Time.deltaTime*rotationThisFrame);
+        rb.freezeRotation= false; //unfreezing rotation so physics system work
     }
 }
